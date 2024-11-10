d6eef087391db41dbbdee7f9e6df7269c4b5b937
jyotibhalla25 <jyoti25bhalla@gmail.com>
2024-11-10 16:23:36 +0530

    menubar

--- portfolio_Project/src/app/app.module.ts
+++ portfolio_Project/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MenuModule } from 'primeng/menu';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule,MenuModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule,MenubarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- portfolio_Project/src/app/header/header.component.ts
+++ portfolio_Project/src/app/header/header.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
-
+import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'Home',
+              icon: 'pi pi-home',
+               routerLink: '/'
+          },
+          {
+              label: 'Features',
+              icon: 'pi pi-star',
+               routerLink: '/'
+          },
+          {
+              label: 'Projects',
+              icon: 'pi pi-search',
+              items: [
+                  {
+                      label: 'Components',
+                      icon: 'pi pi-bolt'
+                  },
+                  {
+                      label: 'Blocks',
+                      icon: 'pi pi-server',
+                  },
+                  {
+                      label: 'UI Kit',
+                      icon: 'pi pi-pencil'
+                  },
+                  {
+                      label: 'Templates',
+                      icon: 'pi pi-palette',
+                      items: [
+                          {
+                              label: 'Apollo',
+                              icon: 'pi pi-palette'
+                          },
+                          {
+                              label: 'Ultima',
+                              icon: 'pi pi-palette'
+                          }
+                      ]
+                  }
+              ]
+          },
+          {
+              label: 'Contact',
+              icon: 'pi pi-envelope',
+               routerLink: '/'
+
+          }
+      ]
+  }
 
 }
